5eef6bb91663a4c7f52b6f38c43ae0d0df64bd92
Bernhard Schaaf-Christmann <brs@dialogika.de>
2017-05-19 16:57:46 +0200

    Linear feedback shift register mit Periode 65535 hinzu.

--- engine.js
+++ engine.js
@@ -1,9 +1,27 @@
+function check_lfsr_period() { // linear feedback shift register
+	var start_state = 0xACE1;  /* Any nonzero start state will work. */
+    var lfsr = start_state;
+    var bit;                    /* Must be 16bit to allow bit<<15 later in the code */
+    var period = 0;
+
+    do {
+        /* taps: 16 14 13 11; feedback polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
+        bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+        lfsr =  (lfsr >> 1) | (bit << 15);
+        ++period;
+    } while (lfsr != start_state);
+
+    return period;
+}
+
 function main() {
 	console.log("HI");
 	var text_block = document.querySelector('#text-block');
 	console.log(text_block);
 	if (text_block) {
-		text_block.textContent = "INFO";
+		text_block.textContent = "Starte Berechnung…";
+		var period = check_lfsr_period();
+		text_block.textContent = "Linear Feedback Shift Register-Periode = "+period;
 	}
 }
 
