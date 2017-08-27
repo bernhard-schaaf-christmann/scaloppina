38791a1f2d55eb685aa8c31d0511054201704b0e
Bernhard Schaaf-Christmann <brs@dialogika.de>
2017-08-27 10:53:22 +0200

    Kommentare hinzu.

--- engine.js
+++ engine.js
@@ -1,4 +1,8 @@
-/*****************************************************************************/
+/******************************************************************************
+* Class encapsulating a LFSR in Fibonacci form
+******************************************************************************/
+
+/// Constructor: seed is starting value
 Linear_Feedback_Shift_Register = function(seed) {
 	if (!seed) {
 		seed = 1;
@@ -6,6 +10,7 @@ Linear_Feedback_Shift_Register = function(seed) {
 	this.lfsr = parseInt(seed);
 }
 
+/// do one step in LFSR, changes inner state of LFSR
 Linear_Feedback_Shift_Register.prototype.step = function(number) {
 	if (!number) {
 		number = 1;
@@ -28,10 +33,12 @@ Linear_Feedback_Shift_Register.prototype.step = function(number) {
 	return this.lfsr;
 };
 
+/// get inner state
 Linear_Feedback_Shift_Register.prototype.get = function() {
 	return this.lfsr;
 };
 
+/// set inner state, can be used for reseed/restart
 Linear_Feedback_Shift_Register.prototype.set = function(seed) {
 	if (!seed) {
 		seed = 1;
@@ -39,10 +46,10 @@ Linear_Feedback_Shift_Register.prototype.set = function(seed) {
 	this.lfsr = parseInt(seed);
 };
 
-/*****************************************************************************/
-
-
-/*****************************************************************************/
+/******************************************************************************
+* Methods for encoding and decoding arbitrary octet streams.
+******************************************************************************/
+/// constructor: LFSR seed as input
 Transcoder = function(seed) {
 	if (!seed) {
 		seed = 0xACE1; /* Any nonzero start state will work. */
@@ -53,6 +60,7 @@ Transcoder = function(seed) {
 	this.lfsr = this.LFSR.get();
 }
 
+/// helper method for checking period of LFSR
 Transcoder.prototype.step_check_lfsr_period = function() { // linear feedback shift register
 	var lfsr = this.LFSR.step();
 	++this.period;
@@ -62,20 +70,23 @@ Transcoder.prototype.step_check_lfsr_period = function() { // linear feedback sh
 	return {"finished": false, "period": this.period, "lfsr": lfsr};
 }
 
+/// resetting LFSR
 Transcoder.prototype.reset = function() {
 	this.LFSR.set(this.start_state);
 }
 
+/// encode some message, including conversion to octet stream
 Transcoder.prototype.encode = function(message) {
 	var str = message.toString();
 	var result = Uint8Array.from(str);
 	for (var i = 0, len = result.length; i < len; i++) {
-//		result[i] = i;//32+str[i];
+//		result[i] = i;//32+str[i]; // TODO doesn't work as expected??
 	}
 	return result.toString();
 }
 /*****************************************************************************/
 
+/// our entry point
 function main() {
 	console.log("HI");
 	var text_block = document.querySelector('#text-block');
@@ -90,13 +101,14 @@ function main() {
 
 	var transcoder = new Transcoder();
 	var tick = function() {
-		var boost = 256;
+		var boost = 256; // we don't want to show every single step, to slow
 		var result = transcoder.step_check_lfsr_period();
 		while (boost > 0 && !result.finished) {
 			result = transcoder.step_check_lfsr_period();
 			--boost;
 		}
 		if (!result.finished) {
+			/// still some string juggling necessary because of inconveniences of underlying transcoder
 			show("Berechnung läuft…<br>Linear Feedback Shift Register-Wert = 0x"+("000" + result.lfsr.toString(16)).slice(-4)+"<br>Schritte: "+result.period);
 			setTimeout(tick, 1);
 		} else {
