1b356078c9f833ac741f1385a7f5cd97ae008048
Bernhard Schaaf-Christmann <brs@dialogika.de>
2017-09-06 14:26:32 +0200

    CSS dazu. Unnötige Bildereinträge entfernt. Hashes hinzu. Wandern: Wegpunkt 71, 78, 2821 teilweise dazu, sind noch auszuformulieren.

--- wandern_data.js
+++ wandern_data.js
@@ -3,7 +3,6 @@ var quiz_data = {
         "password" : "5,3",
         "password_hash": "6G38",
         "text": "Schachttiefste in m NN",
-        "image": "empty.png",
         "next" : "waypoint_a"
     },
     "waypoint_a": { // 60
@@ -12,43 +11,38 @@ var quiz_data = {
         "quiz_title" : "Spuren, Geister, Zeichen",
         "intro": "An der ersten Einmündung nach dem Tor, das sich von Geisterhand zu bewegen scheint, befinden sich links vier technische Schilder.",
         "text": "Was kennzeichnen die Schilder? Von links nach rechts, nenne nur die vier Anfangsbuchstaben (Kleinschreibung).",
-        "image": "empty.png",
         "next" : "waypoint_b"
     },
     "waypoint_b": { // 61
         "password" : "EN124",
-        "password_hash": "",
+        "password_hash": "iENY92I=",
         "quiz_title" : "",
         "intro": "Folge der Spur des Lichtes ein paar Schritte. Vielleicht Siehste Es - (oder au) Net",
         "text": "Die Antwort hat zwei Buchstaben und 3 Ziffern",
-        "image": "empty.png",
         "next" : "waypoint_c"
     },
     "waypoint_c": { // 62
         "password" : "H",
-        "password_hash": "",
+        "password_hash": "Mg==",
         "quiz_title" : "",
         "intro": "Zwischen zwei Häusern sitzt ein kleiner, alter Mann auf der Erde.",
         "text": "Welcher Buchstabe schwebt über dem Mann?",
-        "image": "empty.png",
         "next" : "waypoint_d"
     },
     "waypoint_d": { // 63
         "password" : "-17",
-        "password_hash": "",
+        "password_hash": "JbV/",
         "quiz_title" : "",
         "intro": "Der moderne Marterpfahl der Straßenverwaltung wartet auf dich an einer Kreuzung.",
         "text": "Löse die Rechenaufgabe fast ganz oben.",
-        "image": "empty.png",
         "next" : "waypoint_e"
     },
     "waypoint_e": { // 64
-        "password" : "27",
-        "password_hash": "",
+        "password" : "25",
+        "password_hash": "hZc=",
         "quiz_title" : "",
         "intro": "Vor einem Haus mit auffallender Farbe steht ein Kasten.", // TODO Zu einfach
         "text": "Bilde die Quersumme der Ziffern.",
-        "image": "empty.png",
         "next" : "waypoint_g" // Schachtdeckel hatten wir schon
     },
     "waypoint_f": { // 65
@@ -62,39 +56,51 @@ var quiz_data = {
     },
     "waypoint_g": { // 66
         "password" : "A",
-        "password_hash": "",
+        "password_hash": "UA==",
         "quiz_title" : "",
         "intro": "Auf das größte Shopping-Center weißt eine Tafel aus purem Marmor hin.",
         "text": "Wie lautet der hexadezimale Tag?",
-        "image": "empty.png",
         "next" : "waypoint_h"
     },
     "waypoint_h": { // SaarErlebnisland
         "password" : "267",
-        "password_hash": "",
+        "password_hash": "EshB",
         "quiz_title" : "",
         "intro": "Überquere den Platz und folge der Fußgängerstraße bis zu einem Erlebnis, das neben einem Helden steht.",
         "text": "Welche Hausnummer hat das erwähnte Jagdhaus?",
-        "image": "empty.png",
         "next" : "waypoint_i"
     },
     "waypoint_i": { // Heldenstatue
         "password" : "-10",
-        "password_hash": "",
+        "password_hash": "xbER",
         "quiz_title" : "",
         "intro": "Der Held brütet über zwei Rechenaufgaben.",
         "text": "Addiere die Ergebnisse.",
-        "image": "empty.png",
         "next" : "waypoint_j"
     },
     // Die restlichen Bilder konnte ich ohne Karte nicht mehr zuordnen.
-    "waypoint_j": { // 68
+    "waypoint_j": { // 71
         "password" : "",
         "password_hash": "",
         "quiz_title" : "",
-        "intro": "",
+        "intro": "In Richtung Gotteshaus – in Richtung Himmel", // TODO
         "text": "",
-        "image": "empty.png",
+        "next" : "finish"
+    },
+    "waypoint_k": { // 78…82
+        "password" : "MAHNUNG", // TODO
+        "password_hash": "",
+        "quiz_title" : "",
+        "intro": "Mutter und Kind, Jugend und …", // TODO besser formulieren
+        "text": "",
+        "next" : "finish"
+    },
+    "waypoint_l": { // 2821
+        "password" : "21451730", // Kameraseriennummer
+        "password_hash": "",
+        "quiz_title" : "",
+        "intro": "Autofriedhof. Die Legende besagt hier wurde ein Bus eingegraben. Wie lautet die unbedeutende Zahl auf der Fahrerseite?",
+        "text": "",
         "next" : "finish"
     },
     "finish": {
