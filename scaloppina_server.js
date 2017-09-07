b70e2b7c61527c14112d6af136276ab66295f293
Bernhard Schaaf-Christmann <brs@dialogika.de>
2017-09-07 22:59:03 +0200

    Server wertet die Anteile an richtigen Lösungen pro Location aus und gibt ein Ergebnis aus, inklusive Sättigung, für eine flexible Teilnehmerzahl.

--- scaloppina_server.js
+++ scaloppina_server.js
@@ -5,15 +5,25 @@ var http = require("http"),
 	fs = require("fs"),
 	port = process.argv[2] || 8888;
 
-const SERVE_DIR = "app_files/"
+const SERVE_DIR = "app_files/";
+const NUM_PLAYERS = 11;
+
+var Transcoder = require("./engine_node.js");
 
 var logn = {
 	info : function() {
-		console.log.apply(this, arguments); // varargs
+		console.log.apply(this, arguments);
+	},
+	debug : function() {
+//		console.log.apply(this, arguments);
 	}
 }
 
+var transcoder = new Transcoder();
+
 var submitted_solutions = {};
+var location_cardinality = 0;
+var results = {};
 
 var merge_solution = function(body) {
 	var solution = JSON.parse(body);
@@ -31,6 +41,7 @@ var merge_solution = function(body) {
 }
 
 var evaluate_solutions = function() {
+	location_cardinality = Object.keys(submitted_solutions).length;
 	for (var locus in submitted_solutions) {
 //		logn.info(locus);
 		(function(location) {
@@ -39,7 +50,7 @@ var evaluate_solutions = function() {
 				if(err) {
 					return;
 				}
-				logn.info("sourcing:", filename);
+				logn.debug("sourcing:", filename);
 				var file_source = file.toString("utf8");
 				eval(file_source);
 				quiz_data_call(location, quiz_data);
@@ -51,14 +62,30 @@ var evaluate_solutions = function() {
 
 var quiz_data_call = function(location, quiz_data) {
 	var cardinality = Object.keys(quiz_data).length-1;
-	console.log(location, "=", quiz_data, "cardinality", cardinality);
+	var sum_strength = 0;
+	logn.debug(location, "=", quiz_data, "cardinality", cardinality);
 	for (var username in submitted_solutions[location]) {
+		var user_points = 0;
 		for (var stage in submitted_solutions[location][username].solutions) {
 			var submitted_password = submitted_solutions[location][username].solutions[stage];
+			var submitted_password_hash = transcoder.encode(submitted_password);
 			var needed_password_hash = quiz_data[stage].password_hash;
-			logn.info("pass:", submitted_password, "needed", needed_password_hash);
+			logn.debug("checking answer: <"+submitted_password+"> hash <"+submitted_password_hash+"> needed <"+needed_password_hash+">");
+			if (submitted_password_hash == needed_password_hash) {
+				user_points += 1;
+			}
 		}
+		var percentage = 100*user_points/cardinality;
+		logn.info("In \""+location+"\" the user "+username+" has "+user_points+"/"+cardinality+" or "+percentage+"%.");
+		if (!results[location]) { results[location] = {}; };
+		if (!results[location][username]) { results[location][username] = {}; };
+		results[location][username].percentage = percentage;
+		sum_strength += percentage;
 	}
+	logn.info("Results for "+location+": "+JSON.stringify(results[location]));
+	var accumulated_success = sum_strength/NUM_PLAYERS/100;
+	logn.info("Accumulated success="+accumulated_success);
+	logn.info("Saturated accumulated success="+Math.tanh(2*accumulated_success));
 };
 
 http.createServer(function(request, response) {
