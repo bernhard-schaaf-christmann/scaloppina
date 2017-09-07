f749e70f966fd1b1e93a1f994f41db4b39f2567d
Christian Schmaltz <cws@dialogika.de>
2017-09-07 11:33:33 +0200

    Fhelende Datei vom letzten commit...

--- app_files/riddle_data.js
+++ app_files/riddle_data.js
@@ -3,7 +3,7 @@ var quiz_data = {
 		"password" : "empathie",
 		"password_hash": "Xj/TxaPV5tN6rSM6mF9gqQ==",
 		"text": "Rätsel 1",
-		"quiz_title" : "Rätsel 1",
+		"quiz_title" : "Rätsel 1: Dialogika-Mitarbeiter",
 		"image": "dia-puzzle.png",
 		"next" : "wahl"
 	},
@@ -11,6 +11,7 @@ var quiz_data = {
 		"password": "sieben",
 		"password_hash": "EUgu27QcyeJ6/E4l",
 		"text": 'Wenn letzte Woche Bundestagswahl gewesen wäre...<br><br><table border="1"><tr><td/><td style="text-align:center">Ergebnis</td><td style="text-align:center">Veränderung zur Vorwoche</td></tr><tr><td style="text-align:center">Union</td><td style="text-align:center">37</td><td style="text-align:center">+18</td></td></tr><tr><td style="text-align:center">AFD</td><td style="text-align:center">11</td><td style="text-align:center">+2</td></tr><tr><td style="text-align:center">Linke</td><td style="text-align:center">9</td><td style="text-align:center">+4</td></tr><tr><td style="text-align:center">FDP</td><td style="text-align:center">8</td><td style="text-align:center">+6</td></tr><tr><td style="text-align:center">Grüne</td><td style="text-align:center">8</td><td style="text-align:center">+3</td></tr><tr><td style="text-align:center">SPD</td><td style="text-align:center">23</td><td style="text-align:center">+9</td></tr></table>',
+		"quiz_title" : "Rätsel 2: Bundestagswahl",
 		"image": "wahl.png",
 		"next" : "papierstapel"
 	},
@@ -18,6 +19,7 @@ var quiz_data = {
 		"password" : "astloch",
 		"password_hash": "kcmyPDsq/NtWiz0nKbk=",
 		"text": "Bei der achten Iteration seines Rätsels passierte es: Ein primitiver Affe und sein Sohn kamen und sprangen wie wahnsinnig auf seine Notizen. Ordentliche Papierstapel wurden von den Primaten rücksichtslos in tausend Richtungen geschleudert. Manche landeten sogar in der Prims! Wie sollte er trotzdem rechtzeitig fertig werden? Als Klassenprimus in Mathe hatte er schließlich einen prima Ruf zu verlieren. Träge sortierte Leopold sein Rätsel über Anfangsbuchstaben wieder ordentlich. Dann der Geistesblitz! Charmanterweise ist hier ein prima Hinweis sechsmal versteckt.",
+		"quiz_title" : "Rätsel 3: Papierstapel",
 		"image": "",
 		"next" : "restodia"
 	},
@@ -25,13 +27,14 @@ var quiz_data = {
 		"password": "streichholzschachtel",
 		"password_hash": "cN5SlAZ7cGFx3EEgb6hG09KcyHbcGOCFuThDTV9EdmbqVFeNqM5c1g==",
 		"text": "",
+		"quiz_title" : "Rätsel 4: Restodia",
 		"image": "RestoDia.png",
 		"next" : "aboutus"
 	},
 	"aboutus": {
 		"password" : "tee",
 		"password_hash": "ov0dd7fm",
-		"quiz_title" : "Über uns",
+		"quiz_title" : "Rätsel 5: Über uns",
 		"intro" : "Über uns<br><br>Wir erfinden Software<br><br>Dazu gehört Können, Phantasie und Mut.<br><br>Seit 1982 bestimmen diesen Werte unseren Stil und prägen unsere Entwicklungen. Zu unseren Kunden entstehen dabei sehr dauerhafte und vertrauensvolle Beziehungen. Gegenseitige Wertschetzung ist Gruntlage dieser effizientn Zusammenarbeit.<br><br>Es gelingt uns, unsere Kunden mit unserem Team zueiner effekktiven Einheit zu verbonden. Einem Team, das auf der Grundlage solider wissenschafticher Ausbildung mit Augenmaß die aktuellsten Entwickungstechnologien einsetzt. Grundsatzentwicklungen, Standardisierung und Normung werden durch unsere Mitarbeit geprägt. Für weltweit führende Softwarepartner erfinden, skizzieren und realisieren wir innovative Lösungen. Davon profitieren unsere Kunden.<br><br>Unsere Kompetenz ist das Können unserer Mitarbeiter. Deren wissenschaftliche Neuqier, perfekte Rechtschreibkenntnisse und langjährige Erfahrung führen zu einem tiefen Verständnis unserer Aufgaben. Unser Tuen dient dem Nutzen unserer Kunden in unterschiedlichen Branchem. Wir verkaufen ihnen solide Produkte als kostengünstige Grundlage erfolgreicher Projekte.",
 		"text" : "Unternehmensdaten<br><br>Geschäftsführer undGesellschafter<br><br>Dr. Bernd H. Schmid<br>Dr. Pete Auler<br>Dr. Jan Messerschmidt<br><br>Geschäftssitz<br><br>DIaLOGIKa — Gesellschaft für angewandt Informatik mbH<br><br>Pascalschacht 1    Telelon +49 6897 935-0<br>66125 Saarbrücken  Telefax +49 6897 935-100<br>Deutschland        E-Mail: ino@dialogika.de<br><br>Register<br>Amtsgericht Saarbrücken<br>Hadelsregister HRB Nr. 7347<br>Ust-ID: DE 138187809<br>Steuer-Nr. 040/107/50569",
 		"image": "gf.jpg",
@@ -39,14 +42,24 @@ var quiz_data = {
 	},
 	"mize": {
 		"password" : "richtig",
+		"quiz_title" : "Rätsel 6: Mize",
 		"password_hash": "mJNBWk4dUw7Qi99+pNo=",
 		"text": "",
 		"image": "mize.png",
-		"next" : "laby"
+		"next" : "mattermost"
 	},
+	"mattermost": {
+		"password" : "ghostbusters",
+		"quiz_title" : "Rätsel 7: Dienstags in E17",
+		"password_hash": "SiOoSAP9ri4BQVNghX9kvBkFmtGJXCXc",
+		"text": "(mehrzahl)",
+		"image": "mattermost.png",
+		"next": "laby"
+    },
 	"laby": {
 		"password" : "teil",
 		"password_hash": "hCg7NwOCloA=",
+		"quiz_title" : "Rätsel 8: Das Labyrinth",
 		"text": "Hinweis: Die Lösung hat vier Buchstaben.",
 		"image": "laby3.png",
 		"next" : "done"
@@ -54,6 +67,7 @@ var quiz_data = {
 	"done": {
 		"password" : "0815",
 		"password_hash": "Y6I5+mlxGzU=",
+		"quiz_title" : "That's All Folks",
 		"text": "Am Ziel",
 		"image": "",
 		"next" : ""
