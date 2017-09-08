04f670ad511394d55ff581bc5abd8f81bf40e57a
Christian Schmaltz <cws@dialogika.de>
2017-09-08 17:22:29 +0200

    Papierstapelraetsel eintfernt und Mathe-Raetsel hinzugefuegt.

--- app_files/riddle_data.js
+++ app_files/riddle_data.js
@@ -11,39 +11,32 @@ var quiz_data = {
 		"text": 'Wenn letzte Woche Bundestagswahl gewesen wäre...<br><br><table border="1"><tr><td/><td style="text-align:center">Ergebnis</td><td style="text-align:center">Veränderung zur Vorwoche</td></tr><tr><td style="text-align:center">Union</td><td style="text-align:center">37</td><td style="text-align:center">+18</td></td></tr><tr><td style="text-align:center">AFD</td><td style="text-align:center">11</td><td style="text-align:center">+2</td></tr><tr><td style="text-align:center">Linke</td><td style="text-align:center">9</td><td style="text-align:center">+4</td></tr><tr><td style="text-align:center">FDP</td><td style="text-align:center">8</td><td style="text-align:center">+6</td></tr><tr><td style="text-align:center">Grüne</td><td style="text-align:center">8</td><td style="text-align:center">+3</td></tr><tr><td style="text-align:center">SPD</td><td style="text-align:center">23</td><td style="text-align:center">+9</td></tr></table>',
 		"quiz_title" : "Rätsel 2: Bundestagswahl",
 		"image": "wahl.png",
-		"next" : "papierstapel"
-	},
-	"papierstapel": {
-		"password_hash": "kcmyPDsq/NtWiz0nKbk=",
-		"text": "Bei der achten Iteration seines Rätsels passierte es: Ein primitiver Affe und sein Sohn kamen und sprangen wie wahnsinnig auf seine Notizen. Ordentliche Papierstapel wurden von den Primaten rücksichtslos in tausend Richtungen geschleudert. Manche landeten sogar in der Prims! Wie sollte er trotzdem rechtzeitig fertig werden? Als Klassenprimus in Mathe hatte er schließlich einen prima Ruf zu verlieren. Träge sortierte Leopold sein Rätsel über Anfangsbuchstaben wieder ordentlich. Dann der Geistesblitz! Charmanterweise ist hier ein prima Hinweis sechsmal versteckt.",
-		"quiz_title" : "Rätsel 3: Papierstapel",
-		"image": "",
 		"next" : "restodia"
 	},
 	"restodia": {
 		"password_hash": "cN5SlAZ7cGFx3EEgb6hG09KcyHbcGOCFuThDTV9EdmbqVFeNqM5c1g==",
 		"text": "",
-		"quiz_title" : "Rätsel 4: Restodia",
+		"quiz_title" : "Rätsel 3: Restodia",
 		"image": "RestoDia.png",
 		"next" : "aboutus"
 	},
 	"aboutus": {
 		"password_hash": "ov0dd7fm",
-		"quiz_title" : "Rätsel 5: Über uns",
+		"quiz_title" : "Rätsel 4: Über uns",
 		"intro" : "Wir erfinden Software<br><br>Dazu gehört Können, Phantasie und Mut.<br><br>Seit 1982 bestimmen diesen Werte unseren Stil und prägen unsere Entwicklungen. Zu unseren Kunden entstehen dabei sehr dauerhafte und vertrauensvolle Beziehungen. Gegenseitige Wertschetzung ist Gruntlage dieser effizientn Zusammenarbeit.<br><br>Es gelingt uns, unsere Kunden mit unserem Team zueiner effekktiven Einheit zu verbonden. Einem Team, das auf der Grundlage solider wissenschafticher Ausbildung mit Augenmaß die aktuellsten Entwickungstechnologien einsetzt. Grundsatzentwicklungen, Standardisierung und Normung werden durch unsere Mitarbeit geprägt. Für weltweit führende Softwarepartner erfinden, skizzieren und realisieren wir innovative Lösungen. Davon profitieren unsere Kunden.<br><br>Unsere Kompetenz ist das Können unserer Mitarbeiter. Deren wissenschaftliche Neuqier, perfekte Rechtschreibkenntnisse und langjährige Erfahrung führen zu einem tiefen Verständnis unserer Aufgaben. Unser Tuen dient dem Nutzen unserer Kunden in unterschiedlichen Branchem. Wir verkaufen ihnen solide Produkte als kostengünstige Grundlage erfolgreicher Projekte.",
 		"text" : "Unternehmensdaten<br><br>Geschäftsführer undGesellschafter<br><br>Dr. Bernd H. Schmid<br>Dr. Pete Auler<br>Dr. Jan Messerschmidt<br><br>Geschäftssitz<br><br>DIaLOGIKa — Gesellschaft für angewandt Informatik mbH<br><br>Pascalschacht 1    Telelon +49 6897 935-0<br>66125 Saarbrücken  Telefax +49 6897 935-100<br>Deutschland        E-Mail: ino@dialogika.de<br><br>Register<br>Amtsgericht Saarbrücken<br>Hadelsregister HRB Nr. 7347<br>Ust-ID: DE 138187809<br>Steuer-Nr. 040/107/50569",
 		"image": "gf.jpg",
 		"next" : "mize"
 	},
 	"mize": {
-		"quiz_title" : "Rätsel 6: Mize",
+		"quiz_title" : "Rätsel 5: Mize",
 		"password_hash": "mJNBWk4dUw7Qi99+pNo=",
 		"text": "",
 		"image": "mize.png",
 		"next" : "mattermost"
 	},
 	"mattermost": {
-		"quiz_title" : "Rätsel 7: Dienstags in E17",
+		"quiz_title" : "Rätsel 6: Dienstags in E17",
 		"password_hash": "SiOoSAP9ri4BQVNghX9kvBkFmtGJXCXc",
 		"text": "(mehrzahl)",
 		"image": "mattermost.png",
@@ -51,9 +44,15 @@ var quiz_data = {
     },
 	"laby": {
 		"password_hash": "NHnYx4IKaKcuOw==",
-		"quiz_title" : "Rätsel 8: Das Labyrinth",
+		"quiz_title" : "Rätsel 7: Das Labyrinth",
 		"text": "Hinweis: Die Lösung hat fünf Buchstaben.",
 		"image": "laby3.png",
+		"next" : "math"
+	},
+	"math": {
+		"password_hash": "os89S14d3Kc=",
+		"quiz_title" : "Rätsel 8: Ich hasse Mathe!",
+		"text": "Wie lauten die nächsten 2 Zahlen/4 Ziffern?<br><br>14 91 62 53 64 96 48 11 00 12 11 44 16 91 96 22<br>Lösung ohne Leerzeichen eingeben.",
 		"next" : "done"
 	},
 	"done": {
@@ -67,3 +66,8 @@ var quiz_data = {
 		"next" : ""
 	}
 }
+
+	// "papierstapel": {
+	// 	"password_hash": "kcmyPDsq/NtWiz0nKbk=",
+	// 	"text": "Bei der achten Iteration seines Rätsels passierte es: Ein primitiver Affe und sein Sohn kamen und sprangen wie wahnsinnig auf seine Notizen. Ordentliche Papierstapel wurden von den Primaten rücksichtslos in tausend Richtungen geschleudert. Manche landeten sogar in der Prims! Wie sollte er trotzdem rechtzeitig fertig werden? Als Klassenprimus in Mathe hatte er schließlich einen prima Ruf zu verlieren. Träge sortierte Leopold sein Rätsel über Anfangsbuchstaben wieder ordentlich. Dann der Geistesblitz! Charmanterweise ist hier ein prima Hinweis sechsmal versteckt.",
+	// },
